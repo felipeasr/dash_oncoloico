f9ce591f5ce428467ba8157f66f3dbd57b89222c
felipe <54966505+felipeasr@users.noreply.github.com>
2023-09-13 16:53:32 -0300

    update pages

--- Home.py
+++ Home.py
@@ -8,8 +8,7 @@ import time
 import folium
 import random
 from streamlit_modal import Modal
-import Pages.Dash.po as pagepo
-import Pages.about.about as pageabout
+
 
 
 
@@ -55,11 +54,35 @@ def formata_numero(valor, prefixo = ''):
     return f'{prefixo} {valor:.2f} milhões'
 
 
-Page_cliente = st.sidebar.selectbox(
-    'Dashboards', ['Sobre','Painel Oncologico', 'Sim'], 0)
-if Page_cliente == 'Sobre':
-    #st.experimental_set_query_params()
-    pageabout.pageabout()
-if Page_cliente == 'Painel Oncologico':
-    #st.experimental_set_query_params()
-    pagepo.pagepo()
+
+       
+  # Centralize todos os elementos
+st.header('Bem-vindo aos Dashboards de Oncológia Pediátrica',divider='orange')
+    
+    
+    # Informações sobre a aplicação (centralizado)
+st.markdown("""
+        <div style='text-align: start;'>
+            <p>Os dados contidos nesses dashboards são provenientes do <a href='https://opendatasus.saude.gov.br/'>OPENDATSUS</a>.</p>
+            <p>Os dados do dashboard do painel oncológico estão atualizados até Julho de 2023. Neste dashboard, você encontrará informações sobre a quantidade de casos por ano, local de atendimento, os maiores diagnósticos e tempo de tratamento em cada hospital.</p>
+        </div>
+    """, unsafe_allow_html=True)
+
+    # Centralize o texto
+st.markdown("""
+        <div style='text-align: center;'>
+            <p>Esta é uma versão <strong>Beta</strong> da aplicação desenvolvida pelo <a href='https://ici.ong/'>Instituto do Câncer Infantil</a>.</p>
+        </div>
+    """, unsafe_allow_html=True)
+col1, col2, col3 = st.columns([5,5,5])
+
+with col1:
+     st.write("")
+
+with col2:
+      st.image("images/1.png")
+      
+with col3:
+     st.write("")
+
+
