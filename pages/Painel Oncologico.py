aa38bf7dba57df880cb0a526e275cb26338355b3
felipeasr <felipeassis544@gmail.com>
2023-09-19 11:11:11 -0300

    update

--- pages/Painel Oncologico.py
+++ pages/Painel Oncologico.py
@@ -333,7 +333,7 @@ def exibir_graficos(data, data2):
     st.plotly_chart(fig3, use_container_width=True)
     st.plotly_chart(fig_diagnósticos, use_container_width=True)
     st.plotly_chart(fig_Trat, use_container_width=True)
-    st.plotly_chart(fig_modalidade, use_container_width=False)
+    st.plotly_chart(fig_modalidade, use_container_width=True)
     # st.write(tabela_relacao)
     st.plotly_chart(fig4)
     st.write("Tabela de Contagem de Casos por Diagnóstico e Tempo de Tratamento")
