627af221e424ed23ced29a52e226e09c16f10ca6
felipeasr <felipeassis544@gmail.com>
2023-09-21 11:15:26 -0300

    update cod

--- pages/Painel Oncologico.py
+++ pages/Painel Oncologico.py
@@ -356,20 +356,7 @@ def exibir_graficos(data, data2):
     st.plotly_chart(fig4)
     st.write("Tabela de Contagem de Casos por Diagnóstico e Tempo de Tratamento")
     st.dataframe(tabela_contagem, use_container_width=True)
-     # Divide a tela em duas colunas
-
-
-        
-    st.plotly_chart(fig3, use_container_width=True)
-    st.plotly_chart(fig_diagnósticos, use_container_width=True)
-    st.plotly_chart(fig_Trat, use_container_width=True)
-    st.plotly_chart(fig_modalidade, use_container_width=True)
-    # st.write(tabela_relacao)
-    st.plotly_chart(fig4, use_container_width=True)
-    st.write("Tabela de Contagem de Casos por Diagnóstico e Tempo de Tratamento")
-    st.dataframe(tabela_contagem, use_container_width=True)
-
-
+   
     # Barra lateral para seleção de estado
 st.sidebar.title("Filtros")
 
