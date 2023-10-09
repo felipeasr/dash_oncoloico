8f00603ca84a5525610b8b9c5b85632668e84d5e
felipe <54966505+felipeasr@users.noreply.github.com>
2023-10-09 11:25:17 -0300

    Update Painel Oncologico.py

--- pages/Painel Oncologico.py
+++ pages/Painel Oncologico.py
@@ -43,6 +43,33 @@ st.title('Painel Oncologico :bar_chart:')
 
 st.markdown(''' Os dados estão atualizados até :orange[Julho de 2023]''')
 # Função para filtrar dados com base no estado selecionado
+st.markdown('''<style>
+/* Tooltip container */
+
+/* Tooltip text */
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+ 
+  /* Position the tooltip text - see examples below! */
+  position: absolute;
+  z-index: 1;
+}
+
+/* Show the tooltip text when you mouse over the tooltip container */
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+</style>
+
+<div class="tooltip">Hover over me
+  <span class="tooltiptext">Tooltip text</span>
+</div''', unsafe_allow_html=True)
 
 
 def filtrar_por_estado_diag(data, estado):
